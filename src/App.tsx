885209729368121dc712ed403aba66d21f85079e
Marcelo Neris <marceloaneris@gmail.com>
2023-04-25 19:58:11 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste1 from './Teste1'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste1 />
+    </>
+  )
+}
+
+export default App
