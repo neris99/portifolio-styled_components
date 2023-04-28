feff05570cd94d707d014d915ffd245a015420e8
Marcelo Neris <marceloaneris@gmail.com>
2023-04-28 18:00:22 -0300

    construindo a estrutura e a seção sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,13 +1,19 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas sint
       veniam, neque recusandae maiores delectus libero alias. Fuga ipsum quae
       optio labore dignissimos non? Labore velit saepe aperiam eveniet nobis?
-    </p>
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=neris99&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=neris99&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
