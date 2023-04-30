e5ec6f50ece503dbb0d5e4bb2b9bcb02c372fea7
Marcelo Neris <marceloaneris@gmail.com>
2023-04-30 12:52:10 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,23 @@
+import Avatar from '../../components/Avatar'
+import { FotoPerfil } from '../../components/Avatar/styles'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/neris99.png" />
-    <Titulo fontSize={20}>Marcelo Neris</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marcelo Neris</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        neris99
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
