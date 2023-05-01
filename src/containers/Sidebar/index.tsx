d5ddef13487ee64ce24ddfa95d39ee6a9d214b31
Marcelo Neris <marceloaneris@gmail.com>
2023-05-01 11:05:42 -0300

    finalização de projeto

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,11 +1,14 @@
 import Avatar from '../../components/Avatar'
-import { FotoPerfil } from '../../components/Avatar/styles'
 import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
 import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SideBarContainer>
       <Avatar />
@@ -16,7 +19,7 @@ const Sidebar = () => (
       <Descricao tipo="principal" fontSize={12}>
         Engenheiro front-end
       </Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SideBarContainer>
   </aside>
 )
